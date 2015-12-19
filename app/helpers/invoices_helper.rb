21790e11868d26b9bd00795989a9953c3c3ac6fb
rails-kung-foo <rails.kung.foo@gmail.com>
2015-12-19 16:32:08 +0000

    Displaying Total cost

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,7 +1,14 @@
 module InvoicesHelper
+
+  # Display billing date details
   def display_bill_period
     t('layouts.invoices.bill_details',
       from: ltime(@invoice_json['statement']['period']['from'], format: :date),
       to: ltime(@invoice_json['statement']['period']['to'], format: :date))
+  end
+
+  # Returns billing total amount
+  def total_to_currency
+    number_to_currency(@invoice_json['total'], { unit: '£' })
   end
 end
