f3b7335563be0f9c92b6a5744d6e2601681073d8
rails-kung-foo <rails.kung.foo@gmail.com>
2015-12-19 16:12:09 +0000

    Displaying date
    Displaying billing date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,19 @@
 module ApplicationHelper
+  # Display page title in tab, depending on controller name.
   def display_page_title
     t("layouts.#{ controller.controller_name }.page_title")
   end
 
+  # Builds logo
   def link_to_root_logo
     link_to(content_tag(:span, '', { class: 'navbarLogo', alt: 'Sky Logo' }), root_path, class: 'navbar-brand')
+  end
+
+  # Localises a date/time with guard protection
+  def ltime(dt, hash={})
+    return nil unless dt
+    
+    dt = Time.parse(dt) unless dt.class == Time
+    dt ? l(dt, hash) : nil
   end
 end
--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,2 +1,7 @@
 module InvoicesHelper
+  def display_bill_period
+    t('layouts.invoices.bill_details',
+      from: ltime(@invoice_json['statement']['period']['from'], format: :date),
+      to: ltime(@invoice_json['statement']['period']['to'], format: :date))
+  end
 end
