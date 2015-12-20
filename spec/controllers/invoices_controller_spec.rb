a8bb4817c8fc5ac7a8ce2cdef938b7ec380c4c74
rails-kung-foo <rails.kung.foo@gmail.com>
2015-12-20 13:32:13 +0000

    Controller invoice spec

--- spec/controllers/invoices_controller_spec.rb
+++ spec/controllers/invoices_controller_spec.rb
@@ -19,141 +19,73 @@ require 'rails_helper'
 # that an instance is receiving a specific message.
 
 RSpec.describe InvoicesController, type: :controller do
+  let(:create_invoice){ CustomersBill.new.get_bill }
+  let(:create_invoice_json){ JSON.parse(create_invoice.body) }
+  let(:invalid_json_file){ 'invalid.json' }
 
-  # This should return the minimal set of attributes required to create a valid
-  # Invoice. As you add validations to Invoice, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # InvoicesController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
-
-  describe "GET #index" do
-    it "assigns all invoices as @invoices" do
-      invoice = Invoice.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:invoices)).to eq([invoice])
-    end
-  end
 
   describe "GET #show" do
-    it "assigns the requested invoice as @invoice" do
-      invoice = Invoice.create! valid_attributes
-      get :show, {:id => invoice.to_param}, valid_session
-      expect(assigns(:invoice)).to eq(invoice)
-    end
-  end
-
-  describe "GET #new" do
-    it "assigns a new invoice as @invoice" do
-      get :new, {}, valid_session
-      expect(assigns(:invoice)).to be_a_new(Invoice)
-    end
-  end
-
-  describe "GET #edit" do
-    it "assigns the requested invoice as @invoice" do
-      invoice = Invoice.create! valid_attributes
-      get :edit, {:id => invoice.to_param}, valid_session
-      expect(assigns(:invoice)).to eq(invoice)
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Invoice" do
-        expect {
-          post :create, {:invoice => valid_attributes}, valid_session
-        }.to change(Invoice, :count).by(1)
+    context "with a valid resources" do
+      before :each do
+        create_invoice
+        create_invoice_json
+        get :show
       end
 
-      it "assigns a newly created invoice as @invoice" do
-        post :create, {:invoice => valid_attributes}, valid_session
-        expect(assigns(:invoice)).to be_a(Invoice)
-        expect(assigns(:invoice)).to be_persisted
+      it "is successful" do
+        expect(response.status).to eq 200
       end
 
-      it "redirects to the created invoice" do
-        post :create, {:invoice => valid_attributes}, valid_session
-        expect(response).to redirect_to(Invoice.last)
+      it "renders template :show" do
+        expect(response).to render_template :show
+      end
+
+      context "instance variable @invoice" do
+        it "has the status code: '200'" do
+          expect(assigns(:invoice).code).to eq '200'
+        end
+
+        it "has the requested bill assigned" do
+          expect(assigns(:invoice).body).to eq create_invoice.body
+        end
+      end
+
+      context "instance variable @invoice_json" do
+        it "has the requested bills body assigned" do
+          expect(assigns(:invoice_json)).to eq create_invoice_json
+        end
       end
     end
 
-    context "with invalid params" do
-      it "assigns a newly created but unsaved invoice as @invoice" do
-        post :create, {:invoice => invalid_attributes}, valid_session
-        expect(assigns(:invoice)).to be_a_new(Invoice)
+    context  "with an invalid resources" do
+      before :each do
+        @invalid_json_file = invalid_json_file
+        get :show, billing_file: invalid_json_file
       end
 
-      it "re-renders the 'new' template" do
-        post :create, {:invoice => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested invoice" do
-        invoice = Invoice.create! valid_attributes
-        put :update, {:id => invoice.to_param, :invoice => new_attributes}, valid_session
-        invoice.reload
-        skip("Add assertions for updated state")
+      it "is successful" do
+        expect(response.status).to eq 200
       end
 
-      it "assigns the requested invoice as @invoice" do
-        invoice = Invoice.create! valid_attributes
-        put :update, {:id => invoice.to_param, :invoice => valid_attributes}, valid_session
-        expect(assigns(:invoice)).to eq(invoice)
+      it "renders template :show" do
+        expect(response).to render_template :show
       end
 
-      it "redirects to the invoice" do
-        invoice = Invoice.create! valid_attributes
-        put :update, {:id => invoice.to_param, :invoice => valid_attributes}, valid_session
-        expect(response).to redirect_to(invoice)
-      end
-    end
+      context "instance variable @invoice" do
+        it "has the status code: '404'" do
+          expect(assigns(:invoice).code).to eq '404'
+        end
 
-    context "with invalid params" do
-      it "assigns the invoice as @invoice" do
-        invoice = Invoice.create! valid_attributes
-        put :update, {:id => invoice.to_param, :invoice => invalid_attributes}, valid_session
-        expect(assigns(:invoice)).to eq(invoice)
+        it "@invoice.body has the error message included: 'Cannot GET /#{ @invalid_json_file }'" do
+          expect(assigns(:invoice).body).to include "Cannot GET /#{ @invalid_json_file }"
+        end
       end
 
-      it "re-renders the 'edit' template" do
-        invoice = Invoice.create! valid_attributes
-        put :update, {:id => invoice.to_param, :invoice => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
+      context "instance variable @invoice_json" do
+        it "is not set and returns: nil" do
+          expect(assigns(:invoice_json)).to be nil
+        end
       end
     end
   end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested invoice" do
-      invoice = Invoice.create! valid_attributes
-      expect {
-        delete :destroy, {:id => invoice.to_param}, valid_session
-      }.to change(Invoice, :count).by(-1)
-    end
-
-    it "redirects to the invoices list" do
-      invoice = Invoice.create! valid_attributes
-      delete :destroy, {:id => invoice.to_param}, valid_session
-      expect(response).to redirect_to(invoices_url)
-    end
-  end
-
 end
