6425cbb2cd6610cb93f56b427019ef02783d59fc
rails-kung-foo <rails.kung.foo@gmail.com>
2015-12-20 13:31:46 +0000

    Model invoice specs

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Constants" do
+    context "BILL_ITEM_TYPES" do
+      it "returns an array of sky product types: ['skyStore', 'package', 'callCharges']." do
+        expect(Invoice::BILL_ITEM_TYPES).to match_array %w(skyStore package callCharges)
+      end
+    end
+  end
 end
