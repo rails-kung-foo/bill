70447d003897fa10c4b71bd00cf7db9a59c1789a
rails-kung-foo <rails.kung.foo@gmail.com>
2015-12-20 13:47:20 +0000

    Allowing helper pecs

--- config/application.rb
+++ config/application.rb
@@ -20,14 +20,14 @@ module Bill
     config.generators do |g|
       g.test_framework :rspec,
         :fixtures => true,
-        :view_specs => false,
-        :helper_specs => false,
+        :view_specs => true,
+        :helper_specs => true,
         :routing_specs => false,
         :controller_specs => true,
         :request_specs => true
       g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
